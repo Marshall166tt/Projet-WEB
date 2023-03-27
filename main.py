5548f1240cc7cb08cd6da07691127a1a253dd23f
Marshall166tt <baglieri.gregoire@outlook.fr>
2023-03-27 15:23:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,28 +12,6 @@ app = Flask(__name__)
 # Les différentes pages
 # ------------------
 
-# connecte à la BDD, affecte le mode dictionnaire aux résultats de requêtes et renvoie un curseur
-def connection_bdd():
-	
-	con = lite.connect('BDD.db')
-	con.row_factory = lite.Row
-	
-	return con
-
-#à copier et à modifier pour chaque connexion à la BDD et pour récuperer les données
-# connecte à la BDD et renvoie toutes les lignes de la table personne
-def selection_personnes():
-	
-	conn = connection_bdd()
-	cur = conn.cursor()
-	
-	cur.execute("SELECT nom, prenom, role FROM personnes")
-	
-	lignes = cur.fetchall()
-	
-	conn.close()
-	
-	return lignes
 
 # crée la page web qui affiche la page d'accueil
 @app.route('/Accueil')
@@ -42,18 +20,18 @@ def Accueil():
 	
 	return render_template('Accueil.html') # utilisation du template html accueil
 
-def reset_bdd():
-	con = lite.connect('BDD.db')
-	con.row_factory = lite.Row
-	cur = con.cursor()
-	cur.execute("DELETE FROM COMMANDE") #remet à zéro la table
-	con.commit()
-	cur.execute("UPDATE PIECES SET stock='' ") #remet à jour la table
-	con.commit()
-	lignes = cur.fetchall()
-	con.close()
+@app.route('/AgiLean')
+def AgiLean():
+	return
 
-reset_bdd()
+@app.route('/AgiLog')
+def AgiLog():
+	return
+
+@app.route('/AgiParts')
+def AgiParts():
+	return
+
 
 # ---------------------------------------
 # Lancer le serveur web local Flask
