65dc0d36a676d10da011a23158415204115ba542
Marshall166tt <baglieri.gregoire@outlook.fr>
2023-04-01 17:59:36 +0200

    update

--- main.py
+++ main.py
@@ -20,39 +20,39 @@ def Accueil():
 	return render_template('Accueil.html') # utilisation du template html accueil
 
 #Pages Client
-@app.route('/Client_Commande', methods=['GET'])
+@app.route('/Client_Commande', methods=['GET', 'POST'])
 def Client_Commande():
 	return render_template('Client_Commande.html')
 
-@app.route('/Client_Reception', methods=['GET'])
+@app.route('/Client_Reception', methods=['GET', 'POST'])
 def Client_Reception():
 	return render_template('Client_Reception.html')
 
 #Pages AgiLean
-@app.route('/AgiLean_Matiere', methods=['GET'])
+@app.route('/AgiLean_Matiere', methods=['GET', 'POST'])
 def AgiLean_Matiere():
 	return render_template('AgiLean_Matiere.html')
 
-@app.route('/AgiLean_Information', methods=['GET'])
+@app.route('/AgiLean_Information', methods=['GET', 'POST'])
 def AgiLean_Information():
 	return render_template('AgiLean_Information.html')
 
 #Pages AgiLog
-@app.route('/AgiLog_Matiere', methods=['GET'])
+@app.route('/AgiLog_Matiere', methods=['GET', 'POST'])
 def AgiLog_Matiere():
 	return render_template('AgiLog_Matiere.html')
 
-@app.route('/AgiLog_Information', methods=['GET'])
+@app.route('/AgiLog_Information', methods=['GET', 'POST'])
 def AgiLog_Information():
 	return render_template('AgiLog_Information.html')
 
 #Pages AgiParts
-@app.route('/AgiParts', methods=['GET'])
+@app.route('/AgiParts', methods=['GET', 'POST'])
 def AgiParts():
 	return render_template('AgiParts.html')
 
 #Pages En Savoir Plus
-@app.route('/EnSavoirPlus', methods=['GET'])
+@app.route('/EnSavoirPlus', methods=['GET', 'POST'])
 def SavoirPlus():
 	return render_template('EnSavoirPlus.html')
 
